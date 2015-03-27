8fe7d99b43d0b9d8b8c944aae982c6add51429de
Mateusz Konicki <M.Konicki@lgbs.pl>
2015-03-27 13:32:33 +0100

    RED light tests, wszystkie metody wywalają notimplementedexception

---
+++ Documents/Visual Studio 2012/Projects/Tests/Tests.test/CodeToTest/Calc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tests.test.CodeToTest
+{
+    class Calc
+    {
+        public Calc CreateCalc()
+        {
+            throw new NotImplementedException();
+            
+           // return new Calc();
+        }
+        public int AddTwoInt(int a, int b)
+        {
+            throw new NotImplementedException();
+           // return a + b;
+        }
+        public float SubstractTwoFloat(float a, float b)
+        {
+            throw new NotImplementedException();
+           // return a - b;
+        }
+        public double DivisionTwoDouble(double a, double b)
+        {
+            throw new NotImplementedException();
+            //return a / b;
+        }
+    }
+}
---
+++ Documents/Visual Studio 2012/Projects/Tests/Tests.test/Test/Calc.Tests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Tests.test.CodeToTest;
+
+namespace Tests.test.Test
+{
+      [TestFixture]
+    class CalcTest
+    {
+          [Test]
+          public void CalcIsNotNull()
+          {
+              Calc calc = new Calc();
+              Assert.IsNotNull(calc.CreateCalc());
+          }
+        
+          [TestCase(3, 4, 7)]
+          [TestCase(9, 12, 21)]
+          [TestCase(21, 5, 26)]
+          public void CalcAddIntsTest(int a, int b, int result)
+          {
+              Calc calc = new Calc();
+              Assert.IsInstanceOfType(typeof(int),calc.AddTwoInt(a,b));
+              Assert.AreEqual(result,calc.AddTwoInt(a,b));
+          }
+          [TestCase(3, 4, -1)]
+          [TestCase(9f, 12f, -3f)]
+          [TestCase(21f, 5f, 16f)]
+          public void CalcSubstractTwoFloatTest(float a, float b, float result)
+          {
+              Calc calc = new Calc();
+              Assert.IsInstanceOfType(typeof(float), calc.SubstractTwoFloat(a, b));
+              Assert.AreEqual(result, calc.SubstractTwoFloat(a, b));
+          }
+          [TestCase(8, 4, 2)]
+          [TestCase(12, 9, 0.333d)]
+          [TestCase(14, 3.5, 4)]   
+          public void CalcDivisonTwoDoubleTest(double a, double b, double result)
+          {
+              Calc calc = new Calc();
+              Assert.IsInstanceOfType(typeof(double), calc.DivisionTwoDouble(a, b));
+              Assert.AreEqual(result, calc.DivisionTwoDouble(a, b));
+          }
+         
+
+    }
+}

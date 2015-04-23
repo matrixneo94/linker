f3d5e0b9bcc95cdfafd0cd589feb7526bdb0c35a
Matesz Konicki <mateusz.konicki94@gmail.com>
2015-04-23 12:54:54 +0200

    kod wyczyszcony

---
+++ src/DBContext/LinkerDb.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using FrontenedWeb.Models;
+using WebMatrix.WebData;
+
+namespace FrontenedWeb.DBContext
+{
+    public class LinkerDb:DbContext
+
+    {
+        public LinkerDb()
+            : base("name=DefaultConnection")        
+        {
+
+            Database.SetInitializer<LinkerDb>(new CreateDatabaseIfNotExists<LinkerDb>());
+           
+        }
+        
+        public DbSet<Link> Links { get; set; }
+        public DbSet<UserBase> UsersBase { get; set; }
+    }
+}
